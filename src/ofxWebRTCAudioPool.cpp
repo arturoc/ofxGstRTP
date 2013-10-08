6046a7be85496f5f405753c04881e95dc53764e0
arturo <arturo@openframeworks.cc>
2013-10-08 20:27:02 +0200

    audio pool

---
+++ src/ofxWebRTCAudioPool.cpp
@@ -0,0 +1,42 @@
+/*
+ * ofxWebRTCAudioPool.cpp
+ *
+ *  Created on: Oct 8, 2013
+ *      Author: arturo
+ */
+
+#include "ofxWebRTCAudioPool.h"
+
+ofxWebRTCAudioPool::ofxWebRTCAudioPool() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ofxWebRTCAudioPool::~ofxWebRTCAudioPool() {
+	// TODO Auto-generated destructor stub
+}
+
+
+PooledAudioFrame * ofxWebRTCAudioPool::newFrame(){
+	mutex.lock();
+	if(pool.empty()){
+		mutex.unlock();
+		PooledAudioFrame * frame = new PooledAudioFrame(this);
+		return frame;
+	}else{
+		PooledAudioFrame * frame = pool.front();
+		pool.erase(pool.begin());
+		mutex.unlock();
+		return frame;
+	}
+}
+
+void ofxWebRTCAudioPool::relaseFrame(PooledAudioFrame * frame){
+	frame->pool->returnFrameToPool(frame);
+}
+
+void ofxWebRTCAudioPool::returnFrameToPool(PooledAudioFrame * frame){
+	mutex.lock();
+	pool.push_back(frame);
+	mutex.unlock();
+}
---
+++ src/ofxWebRTCAudioPool.h
@@ -0,0 +1,41 @@
+/*
+ * ofxWebRTCAudioPool.h
+ *
+ *  Created on: Oct 8, 2013
+ *      Author: arturo
+ */
+
+#ifndef OFXWEBRTCAUDIOPOOL_H_
+#define OFXWEBRTCAUDIOPOOL_H_
+
+#include "module_common_types.h"
+#include "ofConstants.h"
+#include <list>
+#include "ofTypes.h"
+
+class ofxWebRTCAudioPool;
+
+class PooledAudioFrame{
+public:
+	PooledAudioFrame(ofxWebRTCAudioPool * pool)
+	:pool(pool){}
+
+	webrtc::AudioFrame audioFrame;
+	ofxWebRTCAudioPool * pool;
+};
+
+class ofxWebRTCAudioPool {
+public:
+	ofxWebRTCAudioPool();
+	virtual ~ofxWebRTCAudioPool();
+
+	PooledAudioFrame * newFrame();
+	static void relaseFrame(PooledAudioFrame * buffer);
+
+private:
+	void returnFrameToPool(PooledAudioFrame * buffer);
+	list<PooledAudioFrame *> pool;
+	ofMutex mutex;
+};
+
+#endif /* OFXWEBRTCAUDIOPOOL_H_ */
